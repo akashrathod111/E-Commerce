87ba67b2b43c7c64961f74be1bdff51760b5af72
Akash Rathod <rathod.akash111@gmail.com>
2024-02-13 16:09:24 +0530

    add the middleware auth in the new crud done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,6 +14,10 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $products = Products::with('category','vendor')->latest()->paginate(5);
--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -11,6 +11,10 @@ class VendorController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $vendors = Vendor::latest()->paginate(5);
