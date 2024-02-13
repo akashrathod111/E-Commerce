94e5338d96d5e489612487c6bb883b2e3a4b1280
Akash Rathod <rathod.akash111@gmail.com>
2024-02-13 19:25:16 +0530

    store frontend added done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,10 @@
                                 </li>
                             @endif
                         @else
+                        <li class="nav-item"><a class="nav-link" href="{{url('/home')}}">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{url('/category')}}">Category</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{url('/vendor')}}">Venodr</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{url('/products')}}">Products</a></li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
